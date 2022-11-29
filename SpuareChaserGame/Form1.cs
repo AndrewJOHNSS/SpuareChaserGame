076a94f28a69a2c228014f4fa4afdc04fed23ffa
Andrew Johnstone <andrjohn647@outlook.com>
2022-11-29 10:57:14 -0500

    update, quaility of life changes and point square moves

--- SpuareChaserGame/Form1.cs
+++ SpuareChaserGame/Form1.cs
@@ -7,23 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Media;
 
 namespace SpuareChaserGame
 {
     public partial class Form1 : Form
     {
-        Random placementPoint = new Random();
-        Random placementSpeed = new Random();
+        Random randgen = new Random();
 
         Rectangle player1 = new Rectangle(50, 200, 20, 20);
         Rectangle player2 = new Rectangle(550, 200, 20, 20);
         Rectangle point = new Rectangle(175, 200, 10, 10);
         Rectangle speed = new Rectangle(345, 350, 10, 10);
+        Rectangle border = new Rectangle(1, 1, 600, 400);
 
         int player1Score = 0;
         int player2Score = 0;
 
-        int playerSpeed = 4;
+        SoundPlayer playerbleep = new SoundPlayer(Properties.Resources.Bleep_SoundBible_com_1927126940);
+        SoundPlayer playerBWOWWW = new SoundPlayer(Properties.Resources.foghorn_daniel_simon);
+        SoundPlayer playerHONK = new SoundPlayer(Properties.Resources.Horn_Honk_SoundBible_com_1634776698);
+        SoundPlayer playerSOS = new SoundPlayer(Properties.Resources.sos_morse_code_daniel_simion);
+
+        int pointXSpeed = -6;
+        int pointYSpeed = 6;
+
+        int player1Speed = 4;
+        int player2Speed = 4;
 
         bool wDown = false;
         bool sDown = false;
@@ -35,9 +46,10 @@ namespace SpuareChaserGame
         bool downArrowDown = false;
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
-        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
         SolidBrush greenBrush = new SolidBrush(Color.Green);
-        SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Pen drawpen = new Pen(Color.Red, 5);
         public Form1()
         {
             InitializeComponent();
@@ -109,8 +121,9 @@ namespace SpuareChaserGame
         {
             e.Graphics.FillRectangle(blueBrush, player1);
             e.Graphics.FillRectangle(greenBrush, player2);
-            e.Graphics.FillRectangle(whiteBrush, speed);
-            e.Graphics.FillRectangle(purpleBrush, point);
+            e.Graphics.FillRectangle(yellowBrush, speed);
+            e.Graphics.FillRectangle(whiteBrush, point);
+            e.Graphics.DrawRectangle(drawpen, 0, 0, 600, 400);
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
@@ -118,46 +131,136 @@ namespace SpuareChaserGame
             //move player 1 
             if (wDown == true && player1.Y > 0)
             {
-                player1.Y -= playerSpeed;
+                player1.Y -= player1Speed;
             }
 
             if (sDown == true && player1.Y < this.Height - player1.Height)
             {
-                player1.Y += playerSpeed;
-            }
-            if (aDown == true && player1.Y > 0)
-            {
-                player1.X -= playerSpeed;
+                player1.Y += player1Speed;
             }
 
-            if (dDown == true && player1.Y < this.Height - player1.Height)
+            if (dDown == true && player1.X < this.Width - player2.Width)
             {
-                player1.X += playerSpeed;
+                player1.X += player1Speed;
+            }
+
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= player1Speed;
             }
             //move player 2 
             if (upArrowDown == true && player2.Y > 0)
             {
-                player2.Y -= playerSpeed;
+                player2.Y -= player2Speed;
             }
 
             if (downArrowDown == true && player2.Y < this.Height - player2.Height)
             {
-                player2.Y += playerSpeed;
+                player2.Y += player2Speed;
             }
-            // check score and stop game if either player is at 3 
-            if (player1Score == 3)
+
+            if (rightArrowDown == true && player2.X < this.Width - player2.Width)
             {
+                player2.X += player2Speed;
+            }
+
+            if (leftArrowDown == true && player2.X > 0)
+            {
+                player2.X -= player2Speed;
+            }
+            // check score and stop game if either player is at 10
+            if (player1Score == 10)
+            {
+                playerHONK.Play();
                 gameTimer.Enabled = false;
                 winLabel.Visible = true;
                 winLabel.Text = "Player 1 Wins!!";
+                this.Refresh();
+                Thread.Sleep(4000);
+                winLabel.Text = "bye bye";
+                playerBWOWWW.Play();
+                this.Refresh();
+                Thread.Sleep(400);
+                playerBWOWWW.Play();
+                Application.Exit();
             }
-            else if (player2Score == 3)
+            else if (player2Score == 10)
             {
+                playerSOS.Play();
                 gameTimer.Enabled = false;
                 winLabel.Visible = true;
                 winLabel.Text = "Player 2 Wins!!";
+                this.Refresh();
+                Thread.Sleep(4000);
+                winLabel.Text = "bye bye";
+                playerBWOWWW.Play();
+                this.Refresh();
+                Thread.Sleep(400);
+                playerBWOWWW.Play();
+                Application.Exit();
             }
 
+            //if you hit point you gain point
+            if (player1.IntersectsWith(point))
+            {
+                int x = randgen.Next(0, 580);
+                int y = randgen.Next(0, 380);
+                point.X = x;
+                point.Y = y;
+                player1Score++;
+                pointsPlayer1Label.Text = $"{player1Score}";
+                playerbleep.Play();
+            }
+            if (player2.IntersectsWith(point))
+            {
+                int x = randgen.Next(0, 580);
+                int y = randgen.Next(0, 380);
+                point.X = x;
+                point.Y = y;
+                player2Score++;
+                pointsPlayer2Label.Text = $"{player2Score}";
+                playerbleep.Play();
+            }
+            //if you hit speed, speed increases
+            if (player1.IntersectsWith(speed))
+            {
+                int xspeed = randgen.Next(0, 580);
+                int yspeed = randgen.Next(0, 380);
+                speed.X = xspeed;
+                speed.Y = yspeed;
+                player1Speed++;
+                playerbleep.Play();
+            }
+            if (player2.IntersectsWith(speed))
+            {
+                int xspeed = randgen.Next(0, 580);
+                int yspeed = randgen.Next(0, 380);
+                speed.X = xspeed;
+                speed.Y = yspeed;
+                player2Speed++;
+                playerbleep.Play();
+            }
+
+            //move point all over the place
+            point.X += pointXSpeed;
+            point.Y += pointYSpeed;
+
+            if (point.Y < 0 || point.Y > this.Height - point.Height)
+            {
+                pointYSpeed *= -1;  // or: ballYSpeed = -ballYSpeed; 
+            }
+            if (point.Y > 400)
+            {
+                pointYSpeed *= -1;
+            }
+            if (point.X < 0)
+            {
+                pointXSpeed *= -1;
+            }
+            else if (point.X > 600)
+            {
+                pointXSpeed *= -1;
+            }
             Refresh();
         }
     }
--- SpuareChaserGame/Properties/Resources.Designer.cs
+++ SpuareChaserGame/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SpuareChaserGame.Properties
-{
-
-
+namespace SpuareChaserGame.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,80 @@ namespace SpuareChaserGame.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SpuareChaserGame.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Bleep_SoundBible_com_1927126940 {
+            get {
+                return ResourceManager.GetStream("Bleep_SoundBible_com_1927126940", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream foghorn_daniel_simon {
+            get {
+                return ResourceManager.GetStream("foghorn_daniel_simon", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Horn_Honk_SoundBible_com_1634776698 {
+            get {
+                return ResourceManager.GetStream("Horn_Honk_SoundBible_com_1634776698", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream sos_morse_code_daniel_simion {
+            get {
+                return ResourceManager.GetStream("sos_morse_code_daniel_simion", resourceCulture);
             }
         }
     }
